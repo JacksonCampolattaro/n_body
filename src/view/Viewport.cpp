3f8a2f086468f520c05a94a732fa0df0210cb33d
JacksonCampolattaro <jackcamp@vt.edu>
2020-01-20 17:21:15 -0500

    Fixed issue where Viewport wasn't using Animable bodies correctly.

--- src/view/Viewport.cpp
+++ src/view/Viewport.cpp
@@ -5,6 +5,7 @@
 #include "Viewport.h"
 #include "../model/calculation/Solver.h"
 #include "../model/calculation/BarnesHut/BarnesHutSolver.h"
+
 #include <Magnum/GlmIntegration/Integration.h>
 
 using namespace Magnum;
@@ -65,7 +66,7 @@ Viewport::Viewport(const Arguments &arguments) :
 
     for (Body b : config->bodies) {
 
-        auto bodyObject = new BodyAnimableObject(&manipulator, &b);
+        auto bodyObject = new BodyAnimableObject(&manipulator, &animables, &b);
 
         auto color = Color4::fromSrgb(Vector3(b.getColor().r, b.getColor().g, b.getColor().b), b.getColor().s / 2);
         auto scaling = Matrix4::scaling(Vector3(b.getRadius()));
